9ab64561418467243c15dcb894975d3dcb858e93
kkurni <kkurni@gmail.com>
2014-03-14 17:33:00 +1100

    try to use D3js

---
+++ d3.js/car-Tree/car.js
@@ -0,0 +1,32 @@
+
+{
+ "name": "Car",
+ "children": [
+	{
+	   "name": "Make",
+	   "children": [
+		{"name": "Honda", "size":8341},
+		{"name": "BMW", "size": 25117},
+		{"name": "Holden", "size": 28693},
+		{"name": "Mazda", "size": 11854},
+		{"name": "asd", "size": 13800},
+		{"name": "asdasda", "size": 15163},
+		{"name": "dasdasd", "size": 13805},
+		{"name": "asdasd", "size": 10149}
+		
+	   ]
+	},
+	{
+	   "name": "State",
+	   "children": [
+		{"name": "VIC", "size": 1689},
+		{"name": "ACT", "size": 1376},
+		{"name": "NSW", "size": 6923},
+		{"name": "Queensland", "size": 1590},
+		{"name": "SA", "size": 8737},
+		{"name": "Tazmania", "size": 3754}	
+	   ]
+	}
+  
+ ]
+}
---
+++ d3.js/car-bubble/car2.js
@@ -0,0 +1,8 @@
+{"name": "Honda",
+			"children": [
+				{"name": "Civic", "size": 2000},
+				{"name": "Jazz", "size": 5000},
+				{"name": "X10", "size": 2000},
+				{"name": "Accord", "size": 341}
+			]
+		}
